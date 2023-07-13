7e5cac5599a27a3ef2191b406644967a185ef10b
Mehdi Bouzekri <106405634+mbouzekri@users.noreply.github.com>
2023-07-13 12:23:41 -0400

    Add files via upload

--- hft.py
+++ hft.py
@@ -9,7 +9,7 @@ from alpaca.trading.enums import OrderSide, TimeInForce
 
 #Symbol traded and cooldown between consecutive trades
 symbol = "AAPL"
-cooldown = timedelta(seconds=0.5)
+cooldown = timedelta(seconds=0.36)
 
 #Defining the client, stream, and api
 client = TradingClient(config.API_KEY, config.API_SECRET_KEY, paper=True)
@@ -51,24 +51,17 @@ class Position():
 #Function used to submit an order to the client
 def submit_order(order_symbol, order_qty, order_side, order_time_in_force, data):
     if (data.size < 10): return
-    quote.last_trade_time = datetime.now()
-    order_details = MarketOrderRequest(symbol = order_symbol, 
-                                    qty = order_qty, 
-                                    side = order_side, 
-                                    time_in_force = order_time_in_force)
-    order = client.submit_order(order_details)
-    print(str(order_side) + " :" + str(data.price))
+    try:
+        order_details = MarketOrderRequest(symbol = order_symbol, 
+                                        qty = order_qty, 
+                                        side = order_side, 
+                                        time_in_force = order_time_in_force)
+        order = client.submit_order(order_details)
+        print(str(order_side) + " :" + str(data.price))
+    except Exception as order_exception:
+        print(order_exception)
+        
     
-#Function used to use triple exponential moving average 
-def calculate_tema():
-    historical_data = api.get_bars(symbol, "1D", limit=20).df
-    ema1 = historical_data['close'].ewm(span=20, adjust=True).mean()
-    ema2 = ema1.ewm(span=20, adjust=True).mean()
-    ema3 = ema2.ewm(span=20, adjust=True).mean()
-    tema = (3 * ema1) - (3 * ema2) + ema3
-    tema = 3 * (ema1 - ema2) + ema3
-    return round(tema[0], 2)
-
 #Creating instance of quote and position
 quote = Quote()
 position = Position()
@@ -81,22 +74,12 @@ async def on_quote(data):
 async def on_trade(data):
     if (datetime.now() - quote.last_trade_time < cooldown): return
     
-    elif (position.total_shares < 100 and quote.ask - data.price <= 0.02
-        and quote.bid_size > quote.ask_size * 2
-        and data.price > calculate_tema()):
-        submit_order(symbol, 10, OrderSide.BUY, TimeInForce.DAY, data)
-        
-    elif (position.total_shares < 0 and quote.ask - data.price <= 0.02
-        and quote.bid_size > quote.bid_size * 2):
+    if (position.total_shares < 100 and quote.ask - data.price <= 0.02
+        and quote.bid_size > quote.ask_size * 1.5):
         submit_order(symbol, 10, OrderSide.BUY, TimeInForce.DAY, data)  
-    
-    elif (position.total_shares > -100 and data.price - quote.bid <= 0.02
-        and quote.ask_size > quote.bid_size * 2
-        and data.price < calculate_tema()):
-        submit_order(symbol, 10, OrderSide.SELL, TimeInForce.DAY, data)
                 
-    elif (position.total_shares > 0 and data.price - quote.bid <= 0.02
-        and quote.ask_size > quote.bid_size * 2):
+    if (position.total_shares > -100 and data.price - quote.bid <= 0.02
+        and quote.ask_size > quote.bid_size * 1.5):
         submit_order(symbol, 10, OrderSide.SELL, TimeInForce.DAY, data)       
         
 #Handler for trade updates
